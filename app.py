64d8b70fe1e608167cee2982f6da07fff44512ac
Carlos Guerrero <imcarlosguerrero@gmail.com>
2022-06-10 16:55:19 -0500

    Working on new download module

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import cloudscraper
 import json
+from time import sleep
 
 app = Flask(__name__)
 #app.config["SQALCHEMY_DATABASE_URI"] = "SQLITE:///database/info.db"
@@ -17,9 +18,9 @@ def home():
 def manga():
     return render_template("apartamento.html")
 
-@app.route("/downloadManga/<mangaName>/<int:chapterNumber>/<int:pageNumber>", methods=["GET"])
+@app.route("/readManga/<mangaName>/<int:chapterNumber>/<int:pageNumber>", methods=["GET"])
 
-def downloadManga(mangaName, chapterNumber, pageNumber):
+def readManga(mangaName, chapterNumber, pageNumber):
     naturalPageNumber = pageNumber
     naturalChapterNumber = chapterNumber
     pageNumber -= 1
@@ -29,20 +30,16 @@ def downloadManga(mangaName, chapterNumber, pageNumber):
     print(pageNumber)
 
     if(len(chapterImagesURLArray) - 1 == pageNumber):
-        #Cambio de capitulo
-        chapterImageNext = "/downloadManga/" + mangaName + "/" + str(naturalChapterNumber + 1) + "/" + str(1)
+        chapterImageNext = "/readManga/" + mangaName + "/" + str(naturalChapterNumber + 1) + "/" + str(1)
     else:
-        #Cambio de pagina
-        chapterImageNext = "/downloadManga/" + mangaName + "/" + str(naturalChapterNumber) + "/" + str(naturalPageNumber + 1)
-    #Cambio de capitulo hacia atras
-    #Cuando el numero de 
+        chapterImageNext = "/readManga/" + mangaName + "/" + str(naturalChapterNumber) + "/" + str(naturalPageNumber + 1)
 
     if(pageNumber != 0):
-        chapterImagePrevious = "/downloadManga/" + mangaName + "/" + str(naturalChapterNumber) + "/" + str(naturalPageNumber - 1)
+        chapterImagePrevious = "/readManga/" + mangaName + "/" + str(naturalChapterNumber) + "/" + str(naturalPageNumber - 1)
     elif(pageNumber == 0 and chapterNumber != 0):
-        chapterImagePrevious = "/downloadManga/" + mangaName + "/" + str(naturalChapterNumber - 1) + "/" + str(len(getIndividualChapterImagesArray(mangaName, naturalChapterNumber - 1)))
+        chapterImagePrevious = "/readManga/" + mangaName + "/" + str(naturalChapterNumber - 1) + "/" + str(len(getIndividualChapterImagesArray(mangaName, naturalChapterNumber - 1)))
     elif(pageNumber == 0 and chapterNumber == 0):
-        chapterImagePrevious = "/downloadManga/" + mangaName + "/" + str(naturalChapterNumber) + "/" + str(naturalPageNumber)
+        chapterImagePrevious = "/readManga/" + mangaName + "/" + str(naturalChapterNumber) + "/" + str(naturalPageNumber)
 
     return render_template("reader.html", chapterImage = chapterImage , chapterImageNext = chapterImageNext, chapterImagePrevious = chapterImagePrevious)
 
@@ -50,7 +47,7 @@ def downloadManga(mangaName, chapterNumber, pageNumber):
 
 def descargarManga(mangaName):
     mangaURL = "https://mangahub.io/manga/" + mangaName
-    chapterListURLArray = getChapterList(mangaURL, mangaName)
+    chapterListURLArray = getChapters(mangaURL, mangaName)
     chapterListURLArray = list(reversed(chapterListURLArray))
     resultantList = []
     for element in chapterListURLArray:
@@ -96,20 +93,69 @@ def descargarManga(mangaName):
 
     return render_template("reader.html")
 
-def getChapterList(mangaURL, mangaName):
-    scraper = cloudscraper.create_scraper(browser='chrome', interpreter='nodejs')
-    response = scraper.get(mangaURL)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    images = soup.find_all("a")
-    chapterListURLArray = []
-    for image in images:
-        imageHref = str(image.get("href"))
-        if("https://mangahub.io/chapter/" + mangaName + "/chapter-" in imageHref):
-            chapterListURLArray.append(str(image.get("href")))
-    return chapterListURLArray
+@app.route("/testing/<mangaName>", methods=["GET"])
+
+def testing(mangaName):
+    mangaURL = "https://mangahub.io/manga/" + mangaName
+    latestChapter = getLatestChapter(mangaURL)
+    entireFuckingManga = []
+    for i in range(1, latestChapter + 1):
+        chapterImages = getChapterImages(mangaName, i)
+        print(chapterImages)
+        entireFuckingManga.append(chapterImages)
+    print(entireFuckingManga[0][0])
+    print(entireFuckingManga)
+    downloadChapterImages(entireFuckingManga)
+    
+
+    
+
+    return "hello world"
+
+
+def getLatestChapter(mangaURL):
+    while(True):
+        try:
+            scraper = cloudscraper.create_scraper(browser='chrome', interpreter='nodejs')
+            response = scraper.get(mangaURL)
+            start = "Chapter <!-- -->"
+            end = "</a></span></div><div>"
+            latestChapter = int(response.text[response.text.find(start) + len(start):response.text.rfind(end)])
+        except:
+            continue
+        break
+    return latestChapter
+
+def getChapterImages(mangaName, chapterID):
+    while(True):
+        try:
+            scraper = cloudscraper.create_scraper(browser='chrome', interpreter='nodejs')
+            response = scraper.post("https://api.mghubcdn.com/graphql", data={"query":"{chapter(x:m01,slug:\"" + mangaName + "\",number:" + str(chapterID) + "){id,title,mangaID,number,slug,date,pages,noAd,manga{id,title,slug,mainSlug,author,isWebtoon,isYaoi,isPorn,isSoftPorn,unauthFile,isLicensed}}}"})
+            soup = json.loads(response.text)
+        except:
+            continue
+        chapterImages = []
+        for imageCode in (json.loads(soup["data"]["chapter"]["pages"])).values():
+            chapterImages.append("https://img.mghubcdn.com/file/imghub/" + imageCode)
+        break
+    return chapterImages
+
+def downloadChapterImages(entireFuckingManga):
+    photoCode = 0
+    while(True):
+        for i in range(0, len(entireFuckingManga)):
+            for j in range(0, len(entireFuckingManga[i])):
+                    try:
+                        scraper = cloudscraper.create_scraper(browser='chrome', interpreter='nodejs')
+                        img_data = scraper.get(entireFuckingManga[i][j]).content
+                        photoCode += 1
+                        with open(str(photoCode) + '.jpg', 'wb') as handler:
+                            handler.write(img_data)
+                    except:
+                        continue
+        break
 
 def getIndividualChapterImagesArray(mangaName, chapterNumber):
-    
     scraper = cloudscraper.create_scraper(browser='chrome', interpreter='nodejs')
     response = scraper.post("https://api.mghubcdn.com/graphql", data={"query":"{chapter(x:m01,slug:\"" + mangaName + "\",number:" + str(chapterNumber) + "){id,title,mangaID,number,slug,date,pages,noAd,manga{id,title,slug,mainSlug,author,isWebtoon,isYaoi,isPorn,isSoftPorn,unauthFile,isLicensed}}}"})
     print(response.text)
